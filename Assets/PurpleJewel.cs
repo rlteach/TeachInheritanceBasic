041f4e0a6636d4d97bfb2898a2c170bc59ef5061
Richard Leinfellner <richard@modulo17.com>
2017-02-14 16:25:03 +0000

    Post class

---
+++ Assets/PurpleJewel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PurpleJewel : Jewel {
+    protected override void Start()
+    {
+        base.Start();       //Make sure base method does its initialisation
+        mRB.isKinematic = true;
+    }
+    protected override void ProcessUpdate(bool vOffscreen)
+    {       //Process off screen
+        if (vOffscreen)
+        {
+            transform.position = GameHelper.WrapOnScreen(transform.position);	//This one wraps the screen
+        }
+    }
+}
